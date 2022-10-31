b1a5a4355735c3f5981cee293362e057c873a999
dabeastt <arjeljabuel@gmail.com>
2022-10-31 15:03:12 +0800

    created calculatorassembly assembly, testsuite script

---
+++ Lab10 - Project Starter/Assets/Tests/TestSuite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+        private float result = 0.0f;
+
+        [Test]
+        public void TestAddition()
+        {
+            //use assert class to test conditions
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+
+
+        [UnityTest]
+        public IEnumerator TestUnityAddition()
+        {
+            //assert class to test condition
+            //yield to skip a frame
+            yield return null;
+
+            result = Calculator.CalculatePair(5, 2, "+");
+            Assert.AreEqual(result, 7);
+        }
+    }
+}
